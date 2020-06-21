ed81c1be598e85674956bdef6e6d28be8cebf0ce
Mateusz Pilarczyk <bhakta666@gmail.com>
2020-06-21 23:45:53 +0200

    request in node

--- app.js
+++ app.js
@@ -18,40 +18,54 @@
 
 // http://numbersapi.com/${number}/${type}?json
 
-const fetch = require("node-fetch");
-const arg = process.argv[2];
-let type = "";
+// const fetch = require("node-fetch");
+// const arg = process.argv[2];
+// let type = "";
 
-if (arg.indexOf("--year") === 0) {
-  console.log("szukamy informacji o roku ...");
-  type = "year";
-} else if (arg.indexOf("--math") === 0) {
-  console.log("szukamy informacji o liczbie ...");
-  type = "math";
-} else if (arg.indexOf("--trivia") === 0) {
-  console.log("szukamy liczby-ciekawostki ...");
-  type = "trivia";
-}
-
-const equalSign = arg.search("=");
-// console.log(equalSign);
-if (equalSign === -1) {
-  console.log("nie wpisałeś liczby!");
-}
-const number = arg.slice(equalSign + 1);
-// console.log(number);
-
-// if (number === "" || isNaN(Number(number))) {
-//   console.log("to nie jest liczba");
-//   process.exit();
+// if (arg.indexOf("--year") === 0) {
+//   console.log("szukamy informacji o roku ...");
+//   type = "year";
+// } else if (arg.indexOf("--math") === 0) {
+//   console.log("szukamy informacji o liczbie ...");
+//   type = "math";
+// } else if (arg.indexOf("--trivia") === 0) {
+//   console.log("szukamy liczby-ciekawostki ...");
+//   type = "trivia";
 // }
-fetch(`http://numbersapi.com/${number}/${type}?json`)
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error("oooo coś nie tak: " + response.status);
-    }
-  })
-  .then((response) => console.log(response.text))
-  .catch((err) => console.log("Błąd: ", err));
+
+// const equalSign = arg.search("=");
+// // console.log(equalSign);
+// if (equalSign === -1) {
+//   console.log("nie wpisałeś liczby!");
+// }
+// const number = arg.slice(equalSign + 1);
+// // console.log(number);
+
+// // if (number === "" || isNaN(Number(number))) {
+// //   console.log("to nie jest liczba");
+// //   process.exit();
+// // }
+// fetch(`http://numbersapi.com/${number}/${type}?json`)
+//   .then((response) => {
+//     if (response.ok) {
+//       return response.json();
+//     } else {
+//       throw new Error("oooo coś nie tak: " + response.status);
+//     }
+//   })
+//   .then((response) => console.log(response.text))
+//   .catch((err) => console.log("Błąd: ", err));
+
+// Zadanie 3 - NBP API - REQUEST
+// `http://api.nbp.pl/api/exchangerates/rates/a/${code}/`
+
+const request = require("request");
+
+const validCodes = ["usd", "eur", "gbp", "chf"];
+const code = process.argv[2];
+
+const isValid = validCodes.find((currency) => currency === code) ? true : false;
+console.log(isValid);
+const url = `http://api.nbp.pl/api/exchangerates/rates/a/${code}/?format=json`;
+
+console.log(url);
